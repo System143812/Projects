fcaa6b373e176420984f90450b0b704ee891481f
System143812 <mypubacc0unt@gmail.com>
2025-06-30 17:36:21 +0800

    update game.js

--- spritePractice/game/game.js
+++ spritePractice/game/game.js
@@ -18,69 +18,81 @@ canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 let is_moving = 0;
+let action = 0;
+let recentAction = 0;
 let lastDirection = "down";
 let animationX = [[10, 75], [10, 75, 140, 205, 270, 335, 400, 460]];
 let currentFrame = 0;
 let currentAction;
 let animationFrame = animationX[is_moving][currentFrame];
 let animationY = 140;
-let running = false;
 let frameTime = 0;
-const frameDelay = 20;
+const frameDelay = 12;
 
 canvas.style.border = "solid red 1px";
+
+function run(){
+  
+}
+
+function idle(){
+  
+}
 
 function pressKey(e){
   console.log("is moving");
   if((e.key === "ArrowRight" || e.key === "d") && x <= (canvasWidth - moveSize - spriteWidth)){
     x += moveSize;
     is_moving = 1;
-    if(animationY !== 203){
+    action = 1;
+    if(action !== recentAction){
+      recentAction = 1;
       currentFrame = 0;
     }
     animationY = 203;
-    running = true;
-    return running;
   }
   if((e.key === "ArrowLeft" || e.key === "a") && x >= (0 + moveSize)){
     x -= moveSize;
     is_moving = 1;
-    if (animationY !== 75) {
+    action = 1;
+    if (action !== recentAction) {
+      recentAction = 1;
       currentFrame = 0;
     }
     animationY = 75;
-    running = true;
-    return running;
   }
   if((e.key === "ArrowDown" || e.key === "s") && y <= (canvasHeight - moveSize - spriteHeight)){
     y += moveSize;
     is_moving = 1;
-    if (animationY !== 140) {
+    action = 1;
+    if (action !== recentAction) {
+      recentAction = 1;
       currentFrame = 0;
     }
     animationY = 140;
-    running = true;
-    return running;
   }
   if((e.key === "ArrowUp" || e.key === "w") && y >= (0 + moveSize)){
     y -= moveSize;
     is_moving = 1;
-    if (animationY !== 10) {
+    action = 1;
+    if (action !== recentAction) {
+      recentAction = 1;
       currentFrame = 0;
     }
     animationY = 10;
-    running = true;
-    return running;
+    
   }
-  running = false;
-  return running;
 }
 
 document.addEventListener('keydown', pressKey);
 
 function drawAnimate(){
   //iloloop neto yung buong animaton frame ng sprite's through x
-  is_moving = running ? 1 : 0;
+  if(is_moving === 0 && recentAction === 1){
+    action = 0;
+    recentAction = 0;
+    currentFrame = 0;
+  }
   animationFrame = animationX[is_moving][currentFrame];
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   ctx.drawImage(bgImage, 0, 0, 600, 600);
@@ -98,6 +110,7 @@ function drawAnimate(){
     else if(currentFrame < animationX[is_moving].length -1){
       currentFrame ++;
     }
+    is_moving = 0;
     frameTime = 0;
     console.log("not moving");
   }
